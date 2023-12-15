c3502498a84d4979afeacaecce13507eaba459d2
oko-record <127172569+oko-record@users.noreply.github.com>
2023-12-15 19:24:22 +0900

    create authentication

---
+++ todoapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, TaskListLoginView
+
+urlpatterns = [
+    path("", views.TaskList.as_view(), name = "tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name = "task"),
+    path("create-task/", TaskCreate.as_view(), name = "create-task"),
+    path("edit-task/<int:pk>/", TaskUpdate.as_view(), name = "edit-task"),
+    path("delete-task/<int:pk>/", TaskDelete.as_view(), name = "delete-task"),
+    path("login/", TaskListLoginView.as_view(), name = "login"),
+]
---
+++ todoapp/views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from todoapp.models import Task
+from django.contrib.auth.views import LoginView
+
+# Create your views here.
+class TaskList(ListView):
+    model = Task
+    context_object_name = "tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+class TaskDelete(DeleteView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+    context_object_name = "task"
+
+class TaskListLoginView(LoginView):
+    fields = "__all__"
+    template_name = "todoapp/login.html"
+
+    def get_success_url(self):
+        return reverse_lazy("tasks")
