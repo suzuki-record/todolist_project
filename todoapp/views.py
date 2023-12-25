b0c47928fb6e59a2df95adfe56b00ab253da914e
suzuki-record <127172569+oko-record@users.noreply.github.com>
2023-12-25 14:27:28 +0900

    add css

--- todoapp/views.py
+++ todoapp/views.py
@@ -16,6 +16,8 @@ class TaskList(LoginRequiredMixin, ListView):
     context_object_name = "tasks"
     redirect_field_name = "login"
 
+    queryset = Task.objects.order_by('title')
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["tasks"] = context["tasks"].filter(user = self.request.user)
